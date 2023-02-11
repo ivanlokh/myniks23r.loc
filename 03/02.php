7ce9f671368abc33c9c743bdfd66b7a63c6cd3ef
IvanLokh <ivanlokh@gmail.com>
2023-02-11 11:56:46 +0200

    php1102

---
+++ 03/02.php
@@ -0,0 +1,14 @@
+<?php
+/* This is a section
+   of multi-line comments
+   which will not be
+   interpreted */
+?>
+
+
+<?php
+/* Это область
+ многострочного комментария,
+ которая не будет
+ подвергаться интерпретации */
+?>
---
+++ 03/03.php
@@ -0,0 +1,6 @@
+<!-- Пример 3.3. Три разновидности присваивания значений переменным -->
+<?php
+$mycounter = 1;
+$mystring = "Hello";
+$myarray = array("One", "Two", "Three");
+?>
