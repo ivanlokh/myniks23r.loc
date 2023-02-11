d2074a56d11107f3784baac544d75dbb26b3fd33
Ivan Lokh <ivanlokh@gmail.com>
2023-02-11 17:23:22 +0200

    PC 11021723

--- 03/04_test1.php
+++ 03/04_test1.php
@@ -1,7 +1,27 @@
 <?php //test1.php
 $username = "Fred Smith";
 echo $username;
+
 echo "<br>";
+
 $current_user = $username;
 echo $current_user;
+
+echo "<br>";
+
+// Числовые переменные
+$count = 17;
+echo $count;
+
+echo "<br>";
+
+$count = 17.5;
+echo $count;
+
+echo "<br>";
+
+// Массивы
+$team = array('Bill', 'Mary','Mike','Chris','Anne');
+echo $team [3];  // Эта команда отображает имя Chris
+
 ?>
